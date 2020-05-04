76c3d930044ab7e57eeff55ee18443fc4ae49931
monika-grzeskiewicz <monika.grzeskiewicz@gmail.com>
2020-05-04 12:11:34 +0200

    new

---
+++ 4VMs.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# VM1 -> VM2 -> VM4
+# VM2 -> VM3 -> VM4
+# VM4 -> VM3 -> VM1
+# VM3 -> VM2 -> VM1
+
+
+case $1 in
+
+start)
+
+#	create 4 VMs
+
+./basis-script.sh create_VM VM1
+sleep 30
+./basis-script.sh create_VM_interface VM1 7
+
+./basis-script.sh create_VM VM2 --wait
+#uvt-kvm wait VM2
+sleep 30
+./basis-script.sh create_VM_interface VM2 7
+./basis-script.sh create_VM_interface VM2 8
+
+./basis-script.sh create_VM VM3
+#uvt-kvm wait VM3
+sleep 30
+./basis-script.sh create_VM_interface VM3 7
+./basis-script.sh create_VM_interface VM3 8
+
+./basis-script.sh create_VM VM4
+sleep 30
+#uvt-kvm wait VM4
+./basis-script.sh create_VM_interface VM4 7
+
+
+;;
+stop)
+
+./basis-script.sh destroy_VM VM1
+./basis-script.sh destroy_VM VM2
+./basis-script.sh destroy_VM VM3
+./basis-script.sh destroy_VM VM4
+
+#./basis-script.sh delete_bridge br-1
+#./basis-script.sh delete_bridge br-2
+;;
+esac
